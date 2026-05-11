4fb4f12208cc23dfad80d60b02b221b2433ca619
Tejas Avhad <codingwithtejas7@gmail.com>
2026-05-11 12:06:11 +0530

    refactor: update token generation and response structure in auth controller

--- server/src/modules/auth/auth.controller.js
+++ server/src/modules/auth/auth.controller.js
@@ -1,15 +1,25 @@
 import { createUser, loginUser } from "./auth.service.js";
 import { signupSchema, loginSchema } from "./auth.validator.js";
-import { generateToken } from "../../common/utils/generateToken.js";
+import { generateAccessToken, generateRefreshToken } from "../../common/utils/generateToken.js";
+import { id } from "zod/v4/locales";
+import { use } from "react";
+import { email } from "zod";
 
 export const signup = async (req, res) => {
     try {
         const validatedData = signupSchema.parse(req.body);
         const user = await createUser(validatedData)
+        
+        const safeUser = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            createdAT: user.createdAt,
+        }
     
         res.status(201).json({
             success: true,
-            user
+            user: safeUser
         }) 
     } catch (error) {
         res.status(400).json({
@@ -26,7 +36,8 @@ export const login = async (req, res) => {
 
         const user = await loginUser(validatedData);
 
-        const token = generateToken(user.id)
+        const accessToken = generateAccessToken(user.id)
+        const refreshToken = generateRefreshToken(user.id)
 
         res.cookie("token", token, {
             httpOnly: true,
@@ -34,9 +45,18 @@ export const login = async (req, res) => {
             sameSite: "lax",
         });
 
+        const safeUser = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            createdAt: user.createdAt,
+        };
+
+
         res.status(200).json({
             success: true,
-            user,
+            accessToken,
+            user: safeUser,
         })
     } catch (error) {
         res.status(400).json({
