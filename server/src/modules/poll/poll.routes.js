4b37dbf33f95230340b459a6202e49c3ec6f0036
Tejas Avhad <codingwithtejas7@gmail.com>
2026-05-12 07:15:22 +0530

    feat: add route to retrieve poll details by slug

--- server/src/modules/poll/poll.routes.js
+++ server/src/modules/poll/poll.routes.js
@@ -4,8 +4,14 @@ import { createPoll } from "./poll.controller.js";
 
 import { authMiddleware } from "../../common/middleware/auth.middleware.js";
 
+import { getPublicPoll } from "./poll.controller.js";
+
+
 const router = Router();
 
+
 router.post("/create",authMiddleware,createPoll);
+
+router.get("/:slug", getPublicPoll);
 
 export default router;
