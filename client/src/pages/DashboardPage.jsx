67e76f97afb8db32250a9dd3fb3ed2cd68df440d
Tejas Avhad <codingwithtejas7@gmail.com>
2026-05-13 15:30:09 +0530

    feat: implement DashboardPage to display user's polls with loading state

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom"
+import { getMyPolls } from "../api/poll.api";
 
-const DashboardPage = () => {
-  return (
-    <div>
-      DashboardPage
+const DashboardPage = () =>{
+  const [polls, setPolls] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchPolls = async () => {
+      try {
+        const response = await getMyPolls()
+
+        setPolls(response.data)
+      } catch (error) {
+        console.log(error);
+        
+      }finally{
+        setLoading(false)
+      }
+    }
+    fetchPolls();
+  }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  return(
+   <div className="p-6">
+
+      <div className="flex justify-between items-center mb-6">
+
+        <h1 className="text-2xl font-bold">
+          My Polls
+        </h1>
+
+        <Link to="/create" className="border px-4 py-2">
+          Create Poll
+        </Link>
+      </div>
+
+      <div className="grid gap-4">
+
+        {polls.map((poll) => (
+          <div key={poll.id} className="border p-4 rounded"
+          >
+            <h2 className="text-xl font-semibold">
+              {poll.title}
+            </h2>
+
+            <p>
+              Responses:
+              {" "}
+              {poll.totalResponses}
+            </p>
+
+            <p>
+              Published:
+              {" "}
+              {poll.published
+                ? "Yes"
+                : "No"}
+            </p>
+
+            <p>
+              Expires:
+              {" "}
+              {new Date(
+                poll.expiresAt
+              ).toLocaleString()}
+            </p>
+
+            <div className="flex gap-4 mt-4">
+
+              <Link
+                to={`/poll/${poll.slug}`}
+                className="border px-3 py-1"
+              >
+                Open Poll
+              </Link>
+
+              <Link
+                to={`/poll/${poll.slug}/results`}
+                className="border px-3 py-1"
+              >
+                Results
+              </Link>
+            </div>
+          </div>
+        ))}
+
+      </div>
     </div>
   )
 }
