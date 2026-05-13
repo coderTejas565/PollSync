f7a7648083ac45f35cfb69c1694fa08e57af6224
Tejas Avhad <codingwithtejas7@gmail.com>
2026-05-13 09:59:10 +0530

    feat: add auth API functions

---
+++ client/src/api/auth.api.jsx
@@ -0,0 +1,13 @@
+import api from "../lib/axios";
+
+export const signupUser = async (formData) => {
+    const response = await api.post("/auth/signup",formData)
+
+    return response.data;
+}
+
+export const loginUset = async (formData) => {
+    const response = await api.post("/auth/login",formData)
+
+    return response.data;
+}
--- client/src/pages/PublicPollPage.jsx
+++ client/src/pages/PublicPollPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PublicPollPage = () => {
   return (
     <div>
-      
+      PublicPollPage
     </div>
   )
 }
