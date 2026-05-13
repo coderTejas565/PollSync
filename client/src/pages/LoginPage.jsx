d684d83cd531d0ec5094a8b58f94e88adf5cd113
Tejas Avhad <codingwithtejas7@gmail.com>
2026-05-13 15:06:40 +0530

    feat: implement login functionality with user authentication

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
+
+import { loginUser } from "../api/auth.api";
+
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-  return (
-    <div>
-      LoginPage
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default LoginPage
+  const { register, handleSubmit,} = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await loginUser(data);
+
+      localStorage.setItem( "accessToken", response.accessToken);
+
+      alert("Login successful");
+
+      navigate("/dashboard");
+    } catch (error) {
+      alert( error.response?.data?.message);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-[300px]"
+      >
+        <input type="email" placeholder="Email" {...register("email")} className="border p-2"
+        />
+
+        <input type="password" placeholder="Password" {...register("password")} className="border p-2"
+        />
+
+        <button type="submit" className="border p-2">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,11 +1,8 @@
-import { useForm }
-  from "react-hook-form";
+import { useForm } from "react-hook-form";
 
-import { signupUser }
-  from "../api/auth.api";
+import { signupUser } from "../api/auth.api";
 
-import { useNavigate }
-  from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const SignupPage = () => {
   const navigate = useNavigate();
@@ -32,35 +29,17 @@ const SignupPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
 
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-4 w-[300px]"
-      >
-        <input
-          type="text"
-          placeholder="Name"
-          {...register("name")}
-          className="border p-2"
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-[300px]">
+        <input type="text" placeholder="Name" {...register("name")}className="border p-2"
         />
 
-        <input
-          type="email"
-          placeholder="Email"
-          {...register("email")}
-          className="border p-2"
+        <input type="email" placeholder="Email" {...register("email")} className="border p-2"
         />
 
-        <input
-          type="password"
-          placeholder="Password"
-          {...register("password")}
-          className="border p-2"
+        <input type="password" placeholder="Password" {...register("password")} className="border p-2"
         />
 
-        <button
-          type="submit"
-          className="border p-2"
-        >
+        <button type="submit" className="border p-2">
           Signup
         </button>
       </form>
