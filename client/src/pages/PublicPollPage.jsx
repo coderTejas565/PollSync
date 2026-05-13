815d4b2b13b06577400a928fd1e31006e85f42ad
Tejas Avhad <codingwithtejas7@gmail.com>
2026-05-13 09:31:53 +0530

    feat: add initial page components and routing for user authentication and polls

---
+++ client/src/pages/PublicPollPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PublicPollPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PublicPollPage
---
+++ client/src/routes/index.js
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom"
+
+import LoginPage from "../pages/LoginPage"
+import SignupPage from "../pages/SignupPage"
+import DashboardPage from "../pages/DashboardPage"
+import CreatePollPage from "../pages/CreatePollPage"
+import PublicPollPage from "../pages/PublicPollPage"
+
+export const router = createBrowserRouter([
+    {
+        path: "/login",
+        element: <LoginPage/>
+    },
+    {
+        path: "/signup",
+        element: <SignupPage/>
+    },
+    {
+        path: "/dashboard",
+        element: <DashboardPage/>
+    },
+    {
+        path: "/create",
+        element: <CreatePollPage/>
+    },
+    {
+        path: "/poll/:slug",
+        element: <PublicPollPage/>
+    },
+])
