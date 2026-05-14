14a30e24d3ba83a690188494ae3925a86a68480b
Tejas Avhad <codingwithtejas7@gmail.com>
2026-05-14 17:35:41 +0530

    feat: integrate Socket.IO for real-time polling functionality

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,41 @@
+import http from "http"
+
 import dotenv from "dotenv"
 import app from "./app.js"
+import { Server } from "socket.io"
 
 dotenv.config()
 
+const server = http.createServer(app)
+
+export const io = new Server(server,{
+    cors: {
+        origin: "http:localhost:5173",
+
+        credentials: true,
+    }
+})
+
+io.on("connection",(socket) => {
+    console.log("Socket Connected",socket.id);
+    
+})
+
+socket.on("join-poll",(pollId)=>{
+    socket.join(pollId)
+
+    console.log(`Joined room: ${pollId}`);
+    
+})
+
+socket.on("disconnect", () => {
+    console.log("Socket disconnected");
+    
+})
+
 const PORT = process.env.PORT || 8070;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port:${PORT}`);
     
 })
