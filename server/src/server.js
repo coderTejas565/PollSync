78725b946512739665e2be47ea209c0127e0418c
Tejas Avhad <codingwithtejas7@gmail.com>
2026-05-14 17:40:37 +0530

    fix: correct formatting and improve readability in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,41 +1,42 @@
-import http from "http"
+import http from "http";
 
 import dotenv from "dotenv"
-import app from "./app.js"
-import { Server } from "socket.io"
+import app from "./app.js";
+import { Server }from "socket.io";
 
 dotenv.config()
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-export const io = new Server(server,{
+
+export const io = new Server(server, {
     cors: {
-        origin: "http:localhost:5173",
+      origin:
+        "http://localhost:5173",
 
-        credentials: true,
-    }
-})
+      credentials: true,
+    },
+  });
 
-io.on("connection",(socket) => {
-    console.log("Socket Connected",socket.id);
-    
-})
+io.on( "connection", (socket) => {
+    console.log("Socket connected:",socket.id);
 
-socket.on("join-poll",(pollId)=>{
-    socket.join(pollId)
+    socket.on("join-poll", (pollId) => {
+        socket.join(pollId);
 
-    console.log(`Joined room: ${pollId}`);
-    
-})
+        console.log(`Joined room: ${pollId}`);
+      }
+    );
 
-socket.on("disconnect", () => {
-    console.log("Socket disconnected");
-    
-})
+    socket.on( "disconnect",() => {
+        console.log("Socket disconnected");
+      }
+    );
+  }
+);
 
 const PORT = process.env.PORT || 8070;
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port:${PORT}`);
-    
-})
+  console.log(`Server running on ${PORT}`);
+});
