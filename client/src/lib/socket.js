9be2674a2ce855d110e0a5e53844453e30efe8ed
Tejas Avhad <codingwithtejas7@gmail.com>
2026-05-15 16:58:54 +0530

    fix: update socket connection to use environment variable for API URL

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,5 +1,20 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8070", {
-    withCredentials: true,
-  });
+export const socket =
+  io(
+
+    import.meta.env.VITE_API_URL,
+
+    {
+
+      withCredentials: true,
+
+      transports: [
+        "websocket"
+      ],
+
+      reconnection: true,
+
+    }
+
+);
